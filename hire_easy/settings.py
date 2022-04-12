151d59c544f5fe194c6d7e0f6701697ed6b0f3f7
Neesham <neeshamraghav0@gmail.com>
2022-04-12 22:54:07 +0530

    Made it better

--- hire_easy/settings.py
+++ hire_easy/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sz6gg#ur@e)h*e=o3=o5h#gpm568#^#f35-*im&$3eh@7t68in'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
